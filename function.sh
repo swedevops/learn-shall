48347e19854f87053682caad2aeb138ef868864b
swedev99 <swedevops99@gmail.com>
2023-04-25 14:35:01 +0530

    realpath dirname

--- function.sh
+++ function.sh
@@ -2,7 +2,9 @@ function_name() {
   echo hello world
   echo script_name =$0
   echo first_argument=$1
+  echo second_argument=$2
+  echo third_argument=$3
   echo all_arguments=$*
   echo number of_argument=$#
 }
- function_name
+ function_name swe bhar pras
