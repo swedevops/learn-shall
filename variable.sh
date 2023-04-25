3ed491dbc76a6069230ed26a070d634623a80a5a
swedev99 <swedevops99@gmail.com>
2023-04-25 13:51:40 +0530

    realpath dirname

--- variable.sh
+++ variable.sh
@@ -1,6 +1,6 @@
 
 echo script name =   $0
-ech first argument = $1
-ech second argument = $2
-ech all arguments = $*
-ech number of arguments = $#
+echo first argument = $1
+echo second argument = $2
+echo all arguments = $*
+echo number of arguments = $#
