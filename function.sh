b3588c4e9ae984d55671e7a0abd9a83373bc3b1b
swedev99 <swedevops99@gmail.com>
2023-04-25 14:38:30 +0530

    realpath dirname

--- function.sh
+++ function.sh
@@ -1,3 +1,5 @@
+# A name given to set of commands called as function
+#Declare the funtion
 function_name() {
   echo hello world
   echo script_name =$0
@@ -7,4 +9,7 @@ function_name() {
   echo all_arguments=$*
   echo number of_argument=$#
 }
+
+#call the function
  function_name swe bhar pras
+ # we can send inputs to the functions and we can access them with special variables $1-$n,$*,$#
