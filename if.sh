9dea1a7b1009a7847841473f00e28eddf4a9b851
swedev99 <swedevops99@gmail.com>
2023-05-01 14:30:19 +0530

    realpath dirname

--- if.sh
+++ if.sh
@@ -2,7 +2,7 @@ fruitname=$1
 quantity=$2
 if [ "$fruitname"=="mango" ]
 then
-  echo 'Mango Quantity' - $quantity
+  echo 'Mango  Quantity' - $quantity
   else
     echo fruit does not exist
     fi
